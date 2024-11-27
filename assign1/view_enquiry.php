61541683b1cc721cc835c069b4c5c922d9a402b8
Miyu <164436959+miiyumiin@users.noreply.github.com>
2024-11-27 22:59:29 +0800

    Add files via upload

--- assign1/view_enquiry.php
+++ assign1/view_enquiry.php
@@ -17,56 +17,52 @@
     <main>
 		  <h1>Contribute Table</h1>
       <?php
-        $servername='localhost';
-        $username='root';
-        $password='';
-        $dbname='herbarium';
-
-        $conn=mysqli_connect($servername,$username,$password,$dbname);
+        include("conn.php");
 
         if(!$conn){
           die('failed to connect to db'.mysqli_connect_error());
         }
-        
-        $sql = 'select * from contribution';
-        $query = mysqli_query($conn,$sql);
+        else{
+          $sql = 'select * from contribution';
+          $query = mysqli_query($conn,$sql);
 
-        if(!$query){
-          die('error found'.mysqli_error($conn));
-        }
-
-        echo "
-            <table class='table' border='1px'>
-            <tr>
-            <th>Id</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Street Address</th>
-            <th>City/Town</th>
-            <th>State</th>
-            <th>Postcode</th>
-            <th>Phone No.</th>
-            <th>Tutorial</th>
-            </tr>";
-
-          while ($row = mysqli_fetch_array($query))
-          {
-            echo ' <tr>
-              <td>'.$row['id'].'</td>
-              <td>'.$row['first_name'].'</td>
-              <td>'.$row['last_name'].'</td>
-              <td>'.$row['email'].'</td>
-              <td>'.$row['street_address'].'</td>
-              <td>'.$row['city/town'].'</td>
-              <td>'.$row['state'].'</td>
-              <td>'.$row['postcode'].'</td>
-              <td>'.$row['phone_no'].'</td>
-              <td>'.$row['tutorial'].'</td>
-              </tr>';
+          if(!$query){
+            die('error found'.mysqli_error($conn));
           }
 
-          echo "</table>";
+          echo "
+              <table class='table' border='1px'>
+              <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Street Address</th>
+              <th>City/Town</th>
+              <th>State</th>
+              <th>Postcode</th>
+              <th>Phone No.</th>
+              <th>Tutorial</th>
+              </tr>";
+
+            while ($row = mysqli_fetch_array($query))
+            {
+              echo ' <tr>
+                <td>'.$row['id'].'</td>
+                <td>'.$row['first_name'].'</td>
+                <td>'.$row['last_name'].'</td>
+                <td>'.$row['email'].'</td>
+                <td>'.$row['street_address'].'</td>
+                <td>'.$row['city/town'].'</td>
+                <td>'.$row['state'].'</td>
+                <td>'.$row['postcode'].'</td>
+                <td>'.$row['phone_no'].'</td>
+                <td>'.$row['tutorial'].'</td>
+                </tr>';
+            }
+
+            echo "</table>";
+          }
       ?>
     </main>
   </body>
