e34ae497532d01afc62d3a61fb9823d5b0888000
Miyu <164436959+miiyumiin@users.noreply.github.com>
2024-11-28 18:58:35 +0800

    Update enquiry_process.php

--- assign1/enquiry_process.php
+++ assign1/enquiry_process.php
@@ -9,22 +9,22 @@
     $phone = $_POST['phone'];
     $tutorial = $_POST['tutorial'];
 
-    if (!empty($id))||(!empty($plant_name))||(!empty($plant_family))||(!empty($plant_genus))
-    ||(!empty($plant_species))||(!empty($herbarium_img))||(!empty($fresh_leaf_img)){
+    if ((!empty($fname))||(!empty($lname))||(!empty($email))
+    ||(!empty($street))||(!empty($city))||(!empty($state))||(!empty($postcode))
+    ||(!empty($phone))||(!empty($tutorial))){
         include("conn.php");
 
         if(!$conn){
             die('failed to connect to db'.mysqli_connect_error());
         }
         else{
-            $INSERT = "INSERT into contribution (first_name, last_name, email, 
-            street_address, 'city/town', 'state', postcode, phone_no, tutorial) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $INSERT = "INSERT into enquiry (first_name, last_name, email, 
+            street_address, `city/town`, `state`, postcode, phone_no, tutorial) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
             $stmt = $conn->prepare($INSERT);
             $stmt->bind_param("ssssssiis", $fname, $lname, $email, 
             $street, $city, $state, $postcode, $phone, $tutorial);
-            $stmt->execute();
-
+           
             if ($stmt->execute()){
                 echo "Enquiry data succesfully saved";
             }else{
