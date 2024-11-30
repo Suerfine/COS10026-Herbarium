95ee6ed39faa674dc6b1beac2228032ce6a5a9c5
Miyu <164436959+miiyumiin@users.noreply.github.com>
2024-11-30 20:25:22 +0800

    Update view_enquiry.php

--- view_enquiry.php
+++ view_enquiry.php
@@ -70,6 +70,14 @@
           }
       ?>
     </main>
-    <?php include('footer.inc'); ?>
+    <?php 
+		$sources=[
+			["url"=>"https://fonts.google.com/icons?selected=Material+Symbols+Outlined:close:FILL@0;wght@400;GRAD@0;opsz@24&icon.size=24&icon.color=%23e8eaed","label"=>"Nav X Icon"],
+			["url"=>"https://youtu.be/EgqwdlY9iaQ?si=o7AitaXCotQ07HXe","label"=>"Responsive Navbar"],
+			["url"=>"https://cdnjs.com/libraries/font-awesome","label"=>"Nav Bar Icon"],
+		];
+
+		include('./footer.inc'); 
+	?>
   </body>
 </html>
